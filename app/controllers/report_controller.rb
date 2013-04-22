844ac860ded794b22c3beac8e3729924ec795eb5
Mike Smith <mike.smith.03@gmail.com>
2013-04-21 18:55:37 -0700

    Reports: Default date

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,9 +1,19 @@
+require 'active_support'
+
 class ReportController < ApplicationController
 	before_filter :authenticateAdmin
   def index
 
     @users = User.all
     @status = Status.all
+
+    if params[:end_date] == nil && params[:start_date] == nil
+      current_date = Date.parse(((DateTime.now).strftime('%F')))
+
+      params[:end_date] = current_date.to_s
+      params[:start_date] = current_date.prev_month.to_s
+    end
+
 
     respond_to do |format|
       format.html # index.html.erb
